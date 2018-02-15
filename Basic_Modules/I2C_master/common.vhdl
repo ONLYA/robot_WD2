187ba6da5b0801e76ce127f70dd07c8ba4d8c499
ONLYA <36487580+ONLYA@users.noreply.github.com>
2018-02-15 18:12:33 +0000

    Add files via upload

---
+++ Basic_Modules/I2C_master/common.vhdl
@@ -0,0 +1,8 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package common is
+	type angles is array (natural range <>) of natural range 0 to 180;
+	type values is array (natural range <>) of unsigned(11 downto 0);
+end common;
