5a87d8b58825d53cb1148076963472f164bf0531
ONLYA <36487580+ONLYA@users.noreply.github.com>
2018-02-15 18:14:49 +0000

    This one is the predefined package for servo

--- Basic_Modules/I2C_master/common.vhdl
+++ Basic_Modules/I2C_master/common.vhdl
@@ -3,6 +3,6 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package common is
-	type angles is array (natural range <>) of natural range 0 to 180;
-	type values is array (natural range <>) of unsigned(11 downto 0);
-end common;
+	type angles is array (natural range <>) of natural range 0 to 180; --angle array
+	type values is array (natural range <>) of unsigned(11 downto 0);  --value array
+end common; 
